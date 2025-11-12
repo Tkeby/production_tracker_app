f3e5f4a5576a22556ae2c084a1f4f8d637c65df0
etzahel <tsegaw.worasa@gmail.com>
2025-11-12 11:20:24 +0000

    Comment out signup URL and related link in login template to streamline user authentication process

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # Custom authentication views (using allauth's expected URL names)
     path('login/', views.CustomLoginView.as_view(), name='account_login'),
-    path('signup/', views.CustomSignupView.as_view(), name='account_signup'),
+    # path('signup/', views.CustomSignupView.as_view(), name='account_signup'),
     path('confirm-email/', views.CustomEmailVerificationSentView.as_view(), name='account_email_verification_sent'),
     path('confirm-email/<str:key>/', views.CustomConfirmEmailView.as_view(), name='account_confirm_email'),
     
