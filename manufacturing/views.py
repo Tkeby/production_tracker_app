60acb4d105b65ac4a88e8631b81485ae128254e8
etzahel <tsegaw.worasa@gmail.com>
2025-11-12 09:52:13 +0000

    Update queryset in UpdateProductionRunView to return all ProductionRun objects

--- manufacturing/views.py
+++ manufacturing/views.py
@@ -114,7 +114,7 @@ class UpdateProductionRunView(LoginRequiredMixin, UpdateView):
         return kwargs
     
     def get_queryset(self):
-        return ProductionRun.objects.filter(shift_teamleader=self.request.user)
+        return ProductionRun.objects.all()
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
